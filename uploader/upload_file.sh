860efde39dbf8ef424f596b752cd843dd80b96e4
panc86 <panc86@gmail.com>
2021-10-05 23:31:14 +0200

    simplify uploader to add user selected file to path observed by engine

---
+++ uploader/upload_file.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ue
+
+# define project root directory from current
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+echo "Upload a file.zip to an observed directory."
+echo "Notes: other extensions won't be considered."
+
+read -r -p "path: " file_to_upload
+
+cp "${file_to_upload}" "${CWD}/engine/data"
