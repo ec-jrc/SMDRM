bcd270a9ee856933ef4a563de4dee322b92dec79
panc86 <panc86@gmail.com>
2021-10-11 23:02:11 +0200

    uploader on project level for simple path resolution

--- upload_file.sh
+++ upload_file.sh
@@ -7,7 +7,5 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "Upload a file.zip to an observed directory."
 echo "Notes: other extensions won't be considered."
-
-read -r -p "path: " file_to_upload
-
+read -r -p "(full)path: " file_to_upload
 cp "${file_to_upload}" "${CWD}/engine/uploads"
