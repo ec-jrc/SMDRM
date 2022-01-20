40b0c635e26504f07bfb12b0d733f4e7ef4458e6
Emanuele Panizio <Emanuele.PANIZIO@ext.ec.europa.eu>
2022-01-20 20:44:44 +0100

    remove unused env variables

--- annotators/floods/config.py
+++ annotators/floods/config.py
@@ -10,16 +10,12 @@ class Config:
     Base configuration class. Contains default configuration settings + configuration settings applicable to all environments.
     """
     # Default settings
-    FLASK_ENV = 'development'
     DEBUG = False
     TESTING = False
     WTF_CSRF_ENABLED = True
 
     # Settings applicable to all environments
     SECRET_KEY = os.getenv('SECRET_KEY', default='A very terrible secret key.')
-
-    CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
-    RESULT_BACKEND = os.getenv('RESULT_BACKEND')
 
 
 class DevelopmentConfig(Config):
@@ -32,4 +28,5 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    FLASK_ENV = 'production'
+    pass
+
