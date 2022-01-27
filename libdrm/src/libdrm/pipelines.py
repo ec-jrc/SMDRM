6f9f8198f4938dda355609fc06e411d8934422f9
panc86 <panc86@gmail.com>
2022-01-27 22:38:59 +0100

    minor test updates and import fixes for libdrm

--- libdrm/src/libdrm/pipelines.py
+++ libdrm/src/libdrm/pipelines.py
@@ -8,11 +8,12 @@ class Pipeline:
     def __init__(self, steps: typing.List[typing.Tuple[typing.Callable, typing.Optional[dict]]] = None):
         self.steps = steps or list()
 
-    def add(self, step: t.Iterable, kwargs: dict = None) -> None:
+    def add(self, step: typing.Iterable, kwargs: dict = None) -> None:
         self.steps.append((step, kwargs))
 
-    def execute(self, datapoints: t.Iterable[bytes]) -> t.Iterable:
+    def execute(self, datapoints: typing.Iterable[dict]) -> typing.Iterable[str]:
         iterator = datapoints
         for step, kwargs in self.steps:
             iterator = step(iterator, **kwargs) if kwargs else step(iterator)
         return iterator
+
